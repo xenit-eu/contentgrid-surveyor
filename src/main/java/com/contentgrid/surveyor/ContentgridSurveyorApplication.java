702afe3fe0789969f168cf87c82a522787319a6d
Lars Vierbergen <lars.vierbergen@xenit.eu>
2023-09-29 10:23:37 +0200

    Initial commit

---
+++ src/main/java/com/contentgrid/surveyor/ContentgridSurveyorApplication.java
@@ -0,0 +1,13 @@
+package com.contentgrid.surveyor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContentgridSurveyorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ContentgridSurveyorApplication.class, args);
+	}
+
+}
