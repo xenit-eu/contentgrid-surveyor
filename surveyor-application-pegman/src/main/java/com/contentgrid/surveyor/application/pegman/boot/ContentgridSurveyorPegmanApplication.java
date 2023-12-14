1278d223637501efd6ba4640c2129745a0a59e68
Lars Vierbergen <lars.vierbergen@xenit.eu>
2023-12-14 14:46:26 +0100

    [ACC-1118] Allow access to actuator endpoints under some conditions

--- surveyor-application-pegman/src/main/java/com/contentgrid/surveyor/application/pegman/boot/ContentgridSurveyorPegmanApplication.java
+++ surveyor-application-pegman/src/main/java/com/contentgrid/surveyor/application/pegman/boot/ContentgridSurveyorPegmanApplication.java
@@ -9,6 +9,13 @@ import com.contentgrid.surveyor.spi.config.FindResourceDefinitionsSpiPort;
 import com.contentgrid.surveyor.spi.storage.AggregateMeasurementsSpiPort;
 import com.contentgrid.surveyor.usecase.metrics.FindMetricsUseCase;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.actuate.autoconfigure.security.reactive.EndpointRequest;
+import org.springframework.boot.actuate.autoconfigure.security.reactive.EndpointRequest.EndpointServerWebExchangeMatcher;
+import org.springframework.boot.actuate.autoconfigure.security.servlet.EndpointRequest.EndpointRequestMatcher;
+import org.springframework.boot.actuate.health.HealthEndpoint;
+import org.springframework.boot.actuate.info.InfoEndpoint;
+import org.springframework.boot.actuate.metrics.MetricsEndpoint;
+import org.springframework.boot.actuate.metrics.export.prometheus.PrometheusScrapeEndpoint;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Import;
@@ -16,6 +23,8 @@ import org.springframework.http.HttpMethod;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.web.server.SecurityWebFilterChain;
+import org.springframework.security.web.server.util.matcher.AndServerWebExchangeMatcher;
+import org.springframework.security.web.server.util.matcher.ServerWebExchangeMatcher;
 
 import static org.springframework.security.oauth2.core.authorization.OAuth2ReactiveAuthorizationManagers.hasScope;
 
@@ -46,6 +55,25 @@ public class ContentgridSurveyorPegmanApplication {
     SecurityWebFilterChain springSecurityFilterChain(ServerHttpSecurity http) {
         http
                 .authorizeExchange(exchanges -> exchanges
+                        // requests to the actuators /info, /health, /metrics, and /prometheus are allowed unauthenticated
+                        .matchers(EndpointRequest.to(
+                                InfoEndpoint.class,
+                                HealthEndpoint.class,
+                                MetricsEndpoint.class,
+                                PrometheusScrapeEndpoint.class
+                        )).permitAll()
+                        // requests FROM localhost to actuator endpoints are all permitted
+                        .matchers(new AndServerWebExchangeMatcher(
+                                EndpointRequest.toAnyEndpoint(),
+                                mgmtExchange -> {
+                                    var remoteAddress = mgmtExchange.getRequest().getRemoteAddress();
+                                    if (remoteAddress != null && remoteAddress.getAddress().isLoopbackAddress()) {
+                                        return ServerWebExchangeMatcher.MatchResult.match();
+                                    }
+                                    return ServerWebExchangeMatcher.MatchResult.notMatch();
+                                })
+                        ).permitAll()
+                        // All other GET requests must have scope surveyor:pegman:read
                         .pathMatchers(HttpMethod.GET).access(hasScope("surveyor:pegman:read"))
                         .anyExchange().denyAll()
                 )
