17041d7fd2344983df96b0e36a65ba9906561409
Toon Geens <toon.geens@xenit.eu>
2023-12-19 15:46:06 +0100

    fixing r2dbc auto conf stuff

--- surveyor-application-surveyor/src/main/java/com/contentgrid/surveyor/application/surveyor/autoconfigure/OptionalR2dbcAutoConfiguration.java
+++ surveyor-application-surveyor/src/main/java/com/contentgrid/surveyor/application/surveyor/autoconfigure/OptionalR2dbcAutoConfiguration.java
@@ -1,14 +1,14 @@
-package com.contentgrid.surveyor.application.surveyor.autoconfigure;
-
-import org.springframework.boot.autoconfigure.AutoConfiguration;
-import org.springframework.boot.autoconfigure.AutoConfigureBefore;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
-import org.springframework.boot.autoconfigure.data.r2dbc.R2dbcDataAutoConfiguration;
-import org.springframework.boot.autoconfigure.r2dbc.R2dbcAutoConfiguration;
-
-@AutoConfiguration
-@ConditionalOnProperty(value = "spring.r2dbc.url")
-@AutoConfigureBefore(R2dbcDataAutoConfiguration.class)
-public class OptionalR2dbcAutoConfiguration extends R2dbcAutoConfiguration {
-
-}
+//package com.contentgrid.surveyor.application.surveyor.autoconfigure;
+//
+//import org.springframework.boot.autoconfigure.AutoConfiguration;
+//import org.springframework.boot.autoconfigure.AutoConfigureBefore;
+//import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+//import org.springframework.boot.autoconfigure.data.r2dbc.R2dbcDataAutoConfiguration;
+//import org.springframework.boot.autoconfigure.r2dbc.R2dbcAutoConfiguration;
+//
+//@AutoConfiguration
+//@ConditionalOnProperty(value = "spring.r2dbc.url")
+//@AutoConfigureBefore(R2dbcDataAutoConfiguration.class)
+//public class OptionalR2dbcAutoConfiguration extends R2dbcAutoConfiguration {
+//
+//}
--- surveyor-application-surveyor/src/test/java/com/contentgrid/surveyor/application/surveyor/boot/TestContentgridSurveyorApplication.java
+++ surveyor-application-surveyor/src/test/java/com/contentgrid/surveyor/application/surveyor/boot/TestContentgridSurveyorApplication.java
@@ -4,27 +4,25 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.oauth2.client.registration.InMemoryReactiveClientRegistrationRepository;
 import org.springframework.security.oauth2.client.registration.ReactiveClientRegistrationRepository;
 import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.containers.PostgreSQLR2DBCDatabaseContainer;
 import org.testcontainers.utility.DockerImageName;
 
 @TestConfiguration(proxyBeanMethods = false)
-public class TestContentgridSurveyorApplication {
-
+class TestContentgridSurveyorApplication {
 
     @Bean
     @ServiceConnection
-    static PostgreSQLContainer<?> postgresContainer() {
+    static PostgreSQLContainer postgresContainer() {
         return new PostgreSQLContainer<>(DockerImageName.parse("postgres:latest"));
     }
 
-    @Bean
-    ReactiveClientRegistrationRepository clientRegistrationRepository() {
-        return new InMemoryReactiveClientRegistrationRepository();
-    }
+
 
     public static void main(String[] args) {
         SpringApplication.from(ContentgridSurveyorApplication::main)
