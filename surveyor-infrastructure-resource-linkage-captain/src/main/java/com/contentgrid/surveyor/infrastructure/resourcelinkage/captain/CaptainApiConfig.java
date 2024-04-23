74d590f9e7ff7b1ec665447a639cf51b0dbeff9d
Roel <roel.schevenels@xenit.eu>
2024-04-23 16:57:44 +0200

    Authorize for resource lookups via captain

--- surveyor-infrastructure-resource-linkage-captain/src/main/java/com/contentgrid/surveyor/infrastructure/resourcelinkage/captain/CaptainApiConfig.java
+++ surveyor-infrastructure-resource-linkage-captain/src/main/java/com/contentgrid/surveyor/infrastructure/resourcelinkage/captain/CaptainApiConfig.java
@@ -3,16 +3,32 @@ package com.contentgrid.surveyor.infrastructure.resourcelinkage.captain;
 import java.util.function.Consumer;
 import lombok.Builder;
 import lombok.NonNull;
+import org.springframework.security.oauth2.client.ReactiveOAuth2AuthorizedClientManager;
+import org.springframework.security.oauth2.client.web.reactive.function.client.ServerOAuth2AuthorizedClientExchangeFilterFunction;
 import org.springframework.web.reactive.function.client.WebClient;
 
 @Builder
 public record CaptainApiConfig(
         @NonNull
-        String url
+        String url,
+        String bearer,
+        ReactiveOAuth2AuthorizedClientManager authorizedClientManager
 ) implements Consumer<WebClient.Builder> {
 
     @Override
     public void accept(WebClient.Builder builder) {
-        builder.baseUrl(url);
+        builder.baseUrl(url)
+                .defaultHeaders(httpHeaders -> {
+                    if (bearer != null && authorizedClientManager == null) {
+                        httpHeaders.setBearerAuth(bearer);
+                    }
+                });
+
+        if (authorizedClientManager != null) {
+            var oauth2ClientFilterFunction = new ServerOAuth2AuthorizedClientExchangeFilterFunction(
+                    authorizedClientManager);
+            oauth2ClientFilterFunction.setDefaultClientRegistrationId(bearer);
+            builder.filter(oauth2ClientFilterFunction);
+        }
     }
 }
--- surveyor-infrastructure-resource-linkage-captain/src/main/java/com/contentgrid/surveyor/infrastructure/resourcelinkage/captain/SurveyorResourceLinkageCaptainConfiguration.java
+++ surveyor-infrastructure-resource-linkage-captain/src/main/java/com/contentgrid/surveyor/infrastructure/resourcelinkage/captain/SurveyorResourceLinkageCaptainConfiguration.java
@@ -2,10 +2,12 @@ package com.contentgrid.surveyor.infrastructure.resourcelinkage.captain;
 
 
 import lombok.Data;
+import org.springframework.beans.factory.ObjectProvider;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.hateoas.config.HypermediaWebClientConfigurer;
+import org.springframework.security.oauth2.client.ReactiveOAuth2AuthorizedClientManager;
 import org.springframework.web.reactive.function.client.WebClient;
 
 @Configuration(proxyBeanMethods = false)
@@ -20,16 +22,19 @@ public class SurveyorResourceLinkageCaptainConfiguration {
     CaptainResourceLinkageLookupGateway captainResourceLinkageLookupGateway(
             WebClient.Builder webclientBuilder,
             HypermediaWebClientConfigurer webClientConfigurer,
-            CaptainApiProperties config
+            CaptainApiProperties config,
+            ObjectProvider<ReactiveOAuth2AuthorizedClientManager> oAuth2AuthorizedClientManager
     ) {
         return new CaptainResourceLinkageLookupGateway(webclientBuilder, CaptainApiConfig.builder()
                 .url(config.getUrl())
+                .bearer(config.getBearer())
+                .authorizedClientManager(oAuth2AuthorizedClientManager.getIfAvailable())
                 .build(), webClientConfigurer);
     }
 
     @Data
     private class CaptainApiProperties {
         String url;
-
+        String bearer;
     }
 }
